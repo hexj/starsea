1faad1e7834db89ef1336be7c72b9abb3f34fbbc
吴林 <lin.wu@tongdun.net>
2019-07-07 00:55:47 +0800

    x

--- leocook/py/tushare/monte_carlo/SimpleMonteCarloHdf52.py
+++ leocook/py/tushare/monte_carlo/SimpleMonteCarloHdf52.py
@@ -6,37 +6,29 @@ import numpy as np
 
 # 生成随机数数组
 
-
+# 数据条数
+recodes = 1
 # 把数组拼接成字典，key是date
 
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.max_rows', None)
-#pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
 
-# 天数
-days = 5
 
-# 数据条数
-recodes = 1000000
+e = {
+    't1' : pd.Series([1000]),
+    't2' : pd.Series([1200]),
+    't3' : pd.Series([1000]),
+    't4' : pd.Series([800]),
+    't5' : pd.Series([1000]),
+    't6' : pd.Series([900])
+}
 
-# 定量
-buckNum = 100
-
-# 定额
-buckPrice=100000
-
-# 列
-columns = pd.date_range('20190705', periods=days).map(lambda x: x.strftime('%Y%m%d'))
-
-#print(columns)
-
-# 随机矩阵
-c = np.random.randint(100, 2000, (recodes, days))
-
-# print(c)
+columns = e.keys()
 
 # 矩阵转df
-pdd = pd.DataFrame(c, columns=columns)
+pdd = pd.DataFrame(e)
+
 print(pdd)
 
 print('----------------------------------------------------------------')
@@ -53,7 +45,7 @@ def dingliang(row):
     lastIndex = len(columns) - 1
     for index in range(lastIndex):
         sum = sum + row[index]
-    return row[lastIndex] * lastIndex / sum - 1
+    return row[lastIndex] * (lastIndex) / sum - 1
 
 
 # 定量（平均成本单价）
@@ -62,25 +54,25 @@ def dingliang_price(row):
     lastIndex = len(columns) - 1
     for index in range(lastIndex):
         sum = sum + row[index]
-    return sum / lastIndex
+    return sum / (lastIndex)
 
 
 # 定额（盈利率）
 def dinge(row):
     sum = 0.0
     lastIndex = len(columns) - 1
-    for index in range(len(columns) - 1):
-        sum = sum + 1.0 / row[index]
-    return row[lastIndex] * sum / lastIndex - 1
+    for index in range(lastIndex):
+        sum = sum + (1.0 / row[index])
+    return row[lastIndex] * sum / (lastIndex) - 1
 
 
 # 定额（平均成本单价）
 def dinge_price(row):
     sum = 0.0
     lastIndex = len(columns) - 1
-    for index in range(len(columns) - 1):
+    for index in range(lastIndex):
         sum = sum + 1 / row[index]
-    return lastIndex / sum
+    return (lastIndex) / sum
 
 
 
