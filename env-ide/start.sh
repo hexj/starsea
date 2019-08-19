6e72a86e53dc32ac8330659836ff94b8d1c28f6e
hexj <hexiaojiao@gmail.com>
2019-08-19 16:16:38 +0800

    update mente carlo ipynb and start.sh

--- env-ide/start.sh
+++ env-ide/start.sh
@@ -1,2 +1,24 @@
+#! /bin/bash
+
+echo "当前执行文件......$0"
+function docker_install()
+{
+	echo "检查Docker......"
+	docker -v
+    if [ $? -eq  0 ]; then
+        echo "检查到Docker已安装!"
+    else
+    	echo "请安装docker环境..."
+        echo "mac 环境 url https://download.docker.com/mac/stable/Docker.dmg"
+        echo "windows 环境请访问 https://download.docker.com/win/static/stable/x86_64/"
+        # curl -sSL https://get.daocloud.io/docker | sh
+        # echo "安装docker环境...安装完成!"
+    fi
+    # 创建公用网络==bridge模式
+    #docker network create starsea_network
+}
+
+# 执行函数
+docker_install
 docker-compose up
 docker logs starsea 2>&1 | grep "token" | grep -v "NotebookApp"
