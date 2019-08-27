50b7e0b8685d3e132418e534301478f19d37006d
4v <blue2630@126.com>
2019-08-27 23:58:53 +0800

    add comments in env-ide/taos/start.sh

--- env-ide/taos/start.sh
+++ env-ide/taos/start.sh
@@ -5,6 +5,10 @@ fi
 cd TDengine
 git pull
 cd ..
+#linux/ mac 中如需要用 python 访问该数据 需要从源码安装依赖
 # pip install TDengine/src/connector/python/linux/python3
 docker system prune -f
 docker-compose up 
+
+#docker exec -it taos /bin/bash 
+#/app# ./taos
