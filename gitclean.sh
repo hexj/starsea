e44497bb8ae05166292fc9b2199a8fb3b184e2ea
hexj <hexiaojiao@gmail.com>
2020-01-14 17:02:51 +0800

    merged dev branch and remove dev folders

--- gitclean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-git filter-branch -f --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch '$1'' --tag-name-filter cat -- --all
-# git filter-branch --index-filter 'git rm -r -q --cached --ignore-unmatch '$1'' --prune-empty --tag-name-filter cat -- --all
-
-rm -rf .git/refs/original/
-git reflog expire --expire=now --all
-git gc --prune=now
-git gc  --auto --aggressive --prune=now
--- wblei/tdx/create_table.sql
+++
@@ -1,4 +0,0 @@
-create table stock(stock_timestamp timestamp, stock_code binary(20), stock_date int, stock_open int, stock_close int, stock_low int, stock_high int, stock_amount float, stock_vol int);
-
-
-insert into stock(stock_timestamp, stock_code, stock_date, stock_open, stock_high, stock_low, stock_close, stock_amount, stock_vol) values('2019-08-30 00:57:00', 'sh601878', 20021202, 2340, 2350, 2062, 2096, 311447072.0, 14375927);
